0a7fb7247c1a3aef6b2918fdbbe81bf5a6d25838
rishabhgurbani <rishabh.gurbani23@gmail.com>
2023-01-26 15:47:04 +0530

    new

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "hello world"
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    mon = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT',
+           'NOV', 'DEC']
+    months = [request.form.get(x) for x in mon]
+
+    input_query = np.array(months, dtype=int).reshape(1, 12)
+    prediction = model.predict(input_query)[0]
+
+    return jsonify({'flood': str(prediction)})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
