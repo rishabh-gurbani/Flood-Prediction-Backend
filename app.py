0992f0989ce3323008ac5e32d22d9c99bd0df77f
rishabhgurbani <rishabh.gurbani23@gmail.com>
2023-01-31 19:54:40 +0530

    added support for models

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask, request, jsonify
 import pickle
 import numpy as np
 
-pickle.load(open('model.pkl', 'rb'))
-model = pickle.load(open('model.pkl', 'rb'))
-
 app = Flask(__name__)
 
 
@@ -18,9 +15,26 @@ def predict():
     mon = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT',
            'NOV', 'DEC']
     months = [request.form.get(x) for x in mon]
+    modelType = [request.form.get('MODEL')]
 
     input_query = np.array(months, dtype=float).reshape(1, 12)
+
+    pickle.load(open('LR.pkl', 'rb'))
+    model = pickle.load(open('LR.pkl', 'rb'))
+
+    if modelType == 'LR':
+        pickle.load(open('LR.pkl', 'rb'))
+        model = pickle.load(open('LR.pkl', 'rb'))
+    elif modelType == 'RF':
+        pickle.load(open('RF.pkl', 'rb'))
+        model = pickle.load(open('RF.pkl', 'rb'))
+    elif modelType == 'SVM':
+        pickle.load(open('SVM.pkl', 'rb'))
+        model = pickle.load(open('SVM.pkl', 'rb'))
+
     prediction = model.predict(input_query)[0]
+
+    print(modelType)
 
     return jsonify({'prediction': str(prediction)})
 
